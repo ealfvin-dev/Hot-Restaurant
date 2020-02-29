89197969a9ee303021f575bfe09bb5a2fabed0e2
ClayData <claytonortiz96@gmail.com>
2020-02-29 11:14:51 -0600

    server

--- server.js
+++ server.js
@@ -7,3 +7,31 @@ var PORT = 3000;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+let tables = [];
+let waitList = [];
+
+
+
+
+function reservation() {
+    let newReservation = {
+        name: "",
+        phone: "",
+        email: "",
+        id: ""
+    }
+    if(tables.length < 5){
+        tables.push(newReservation);
+    }
+    else{
+        waitList.push(newReservation);
+    }
+
+    $.post("/api/tables")
+}
+
+
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
