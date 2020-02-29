82b3e7be83fc73f22e6e58742cb767b60d1a1596
Erik Alfvin <erikalfvin@gmail.com>
2020-02-29 13:41:05 -0600

    Figuring out server-side logic

--- server.js
+++ server.js
@@ -10,27 +10,41 @@ app.use(express.json());
 let tables = [];
 let waitList = [];
 
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
+  
+app.get("/form", function(req, res) {
+    res.sendFile(path.join(__dirname, "form.html"));
+});
 
+app.get("/list", function(req, res) {
+    res.sendFile(path.join(__dirname, "list.html"));
+});
 
+app.get("/api/tables", function(req, res) {
+    return res.json(tables);
+});
 
-function reservation() {
-    let newReservation = {
-        name: "",
-        phone: "",
-        email: "",
-        id: ""
-    }
-    if(tables.length < 5){
+app.get("/api/waitList", function(req, res) {
+    return res.json(waitList);
+});
+
+app.post("/api/tables", function(req, res) {
+    let newReservation = req.body;
+    if(tables.length < 5) {
         tables.push(newReservation);
+        res.json(newReservation);
     }
-    else{
+});
+
+app.post("/api/waitList", function(req, res) {
+    let newReservation = req.body;
+    if(tables.length === 5) {
         waitList.push(newReservation);
+        res.json(newReservation);
     }
-
-    $.post("/api/tables")
-}
-
-
+});
 
 app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
